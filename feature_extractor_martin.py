81421253cb61437d996c10263ab4770e64c9a670
mrtnhorn <mrtnhorn@me.com>
2016-06-01 21:22:59 -0700

    Add some more feat combos

--- feature_extractor_martin.py
+++ feature_extractor_martin.py
@@ -56,23 +56,7 @@ def extract_features(e1, e1_b, e1_e, e2, e2_b, e2_e, ace_result, reversed):
         features.extend(get_lemmas(ep_1, ep_2, e1_tag, e2_tag))
         features.extend(get_properties(mrs, ep_1, ep_2, e1_tag, e2_tag))
         features.extend(find_temp_preds(mrs, ep_1, ep_2, e1_tag, e2_tag))
-
-        # Direct argument finding (currently obsolete)
-        args_1 = mrs.args(ep_1.nodeid)
-        args_2 = mrs.args(ep_2.nodeid)
-        feat = None
-
-        for arg in args_1:
-            if ep_2.label == args_1[arg] or args_2['ARG0'] == args_1[arg]:
-                feat = "DIRECT=e1#"+ep_1.pred.pos+"["+arg+"#e2#"+ep_2.pred.pos + "]"
-                #print(feat)
-                features.append(feat)
-        if not feat:
-            for arg in args_2:
-                if ep_1.label == args_2[arg] or args_1['ARG0'] == args_2[arg]:
-                    feat = "DIRECT=e2#"+ep_2.pred.pos+"[#"+arg+"#e1#"+ep_1.pred.pos + "]"
-                    #print(feat)
-                    features.append(feat)
+        features.extend(find_direct_paths(mrs, ep_1, ep_2, e1_tag, e2_tag))
 
         paths = find_paths(mrs, ep_1, ep_2, e1_tag, e2_tag, "")
 
@@ -86,6 +70,28 @@ def extract_features(e1, e1_b, e1_e, e2, e2_b, e2_e, ace_result, reversed):
         features.append("PATH=" + shortest_path)
 
     # print("a6")
+    return features
+
+
+def find_direct_paths(mrs, ep_1, ep_2, e1_tag, e2_tag):
+    features = []
+    # Direct argument finding (currently obsolete)
+    args_1 = mrs.args(ep_1.nodeid)
+    args_2 = mrs.args(ep_2.nodeid)
+    feat = None
+
+    for arg in args_1:
+        if ep_2.label == args_1[arg] or args_2['ARG0'] == args_1[arg]:
+            feat = "DIRECT=" + e1_tag + "#" + ep_1.pred.pos + "[" + arg + "#" + e2_tag + "#" + ep_2.pred.pos + "]"
+            # print(feat)
+            features.append(feat)
+    if not feat:
+        for arg in args_2:
+            if ep_1.label == args_2[arg] or args_1['ARG0'] == args_2[arg]:
+                feat = "DIRECT=" + e2_tag + "#" + ep_2.pred.pos + "[#" + arg + "#" + e1_tag + "#" + ep_1.pred.pos + "]"
+                # print(feat)
+                features.append(feat)
+
     return features
 
 
@@ -195,8 +201,8 @@ def find_paths(mrs, ep_1, ep_2, e1_tag, e2_tag, cur_path, count=0):
                 # Debug line: pred name
                 # cur_path = mrs.ep(node_id).pred.string.strip("\"") + "," + prefix + cur_path
                 paths.append(cur_path)
-        else:
-            paths.clear()
+        # else:
+        #     paths.clear()
 
     return paths
 
@@ -272,7 +278,7 @@ def read_doc(e1, e1_begin, e1_end, e2, e2_begin, e2_end, file_name=None):
 
 
 def write_features_to_file(output_lines):
-    f = open("Data/cachedFeatureDictionary.path-pred-direct.out", "w")
+    f = open("Data/cachedFeatureDictionary.just-properties.out", "w")
     for line in output_lines:
         f.write(line + "\n")
 
